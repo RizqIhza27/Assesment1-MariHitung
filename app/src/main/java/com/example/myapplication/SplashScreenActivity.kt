bee6fcd2e62f8389222a15c5e218f1f460856cdf
RizqIhza27 <rihza27@gmail.com>
2021-03-26 15:34:21 +0700

    membuat home page

---
+++ app/src/main/java/com/example/myapplication/SplashScreenActivity.kt
@@ -0,0 +1,18 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        val handler = Handler()
+        handler.postDelayed({
+            val goGetStarted = Intent(this, HomeActivity::class.java)
+            startActivity(goGetStarted)
+        }, 2000)
+    }
+}
